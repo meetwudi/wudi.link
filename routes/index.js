49efc29fd97cccc1440752dfccc6d1370b44f9c9
John Wu <webmaster@leapoahead.com>
2015-05-19 16:14:39 +0800

    Script initial

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'wudi.link' });
+  res.render('index', { 
+    title: 'wudi.link portal', 
+    scripts: ['app'] 
+  });
 });
 
 module.exports = router;
