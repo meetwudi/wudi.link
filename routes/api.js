35607d6faf3cb39750534459a1f9f3373c7daadf
John Wu <webmaster@leapoahead.com>
2015-05-20 09:46:32 +0800

    secret api

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,25 @@ var router = express.Router();
 
 /* Secret API */
 router.post('/secret', function(req, res) {
-  res.send(req.body);
+  var secret = process.env.SECRET;
+  if (req.body.secret === secret) {
+    req.session.verifed = true;
+    res.json({
+      verified: true
+    });
+  }
+  else {
+    req.session.verified = false;
+    res.json({
+      verified: false
+    });
+  }
+});
+
+router.get('/secret', function(req, res) {
+  res.json({
+    verified: !!req.session.verified
+  });
 });
 
 module.exports = router;
